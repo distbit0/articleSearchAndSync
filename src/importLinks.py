2f8a4c7f385d5cc534931b137740bb61e83a1086
Dist Bit <adp01@pm.me>
2023-09-13 14:22:00 +1000

    remove duplicate non-html files

--- src/importLinks.py
+++ src/importLinks.py
@@ -2,7 +2,8 @@ import json
 import utils
 from utils import getConfig
 import os
-import glob
+from collections import defaultdict
+import hashlib
 
 
 def getBookmarks():
@@ -110,12 +111,59 @@ def deleteDuplicateArticleFiles(urls_to_filenames):
             dir_seen_urls[directory].add(url)
 
 
+def calculate_file_hash(file_path):
+    hasher = hashlib.sha256()
+    file_size = os.path.getsize(file_path)
+
+    if file_size < 4096:
+        with open(file_path, "rb") as f:
+            hasher.update(f.read())
+    else:
+        offset = (file_size - 4096) // 2
+        with open(file_path, "rb") as f:
+            f.seek(offset)
+            hasher.update(f.read(4096))
+
+    return hasher.hexdigest()
+
+
+def find_and_remove_duplicate_files(directory_path):
+    duplicate_size_files = defaultdict(list)
+
+    for root, _, filenames in os.walk(directory_path):
+        if any(part.startswith(".") for part in root.split(os.sep)):
+            continue
+
+        for filename in filenames:
+            full_path = os.path.join(root, filename)
+            file_size = os.path.getsize(full_path)
+            file_hash = calculate_file_hash(full_path)
+            unique_key = f"{file_size}_{file_hash}_{root}"
+
+            duplicate_size_files[unique_key].append(full_path)
+
+    for unique_key, file_paths in duplicate_size_files.items():
+        if len(file_paths) > 1:
+            root_files = [
+                path for path in file_paths if os.path.dirname(path) == directory_path
+            ]
+            non_root_files = [
+                path for path in file_paths if os.path.dirname(path) != directory_path
+            ]
+
+            files_to_remove = root_files if root_files else non_root_files[:-1]
+            for file_path in files_to_remove:
+                print("removed", file_path)
+                os.remove(file_path)
+
+
 if __name__ == "__main__":
     updateUrlListFiles(getConfig()["articleFileFolder"])
     deleteFilesMarkedToDelete()
     moveFilesMarkedToMove()
     articlesAndUrls = utils.getUrlsInLists()
     deleteDuplicateArticleFiles(articlesAndUrls)
+    find_and_remove_duplicate_files(getConfig()["articleFileFolder"])
     utils.addUrlToUrlFile(
         list(articlesAndUrls.values()),
         utils.getAbsPath("../storage/alreadyAddedArticles.txt"),
