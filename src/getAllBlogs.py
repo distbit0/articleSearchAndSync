406e924927ff40c52ce5881d74f6631c25f46bba
Dist Bit <adp01@pm.me>
2024-08-07 08:24:46 +0400

    getAllBlogs.py

--- src/getAllBlogs.py
+++ src/getAllBlogs.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     sortedBlogCounts = sorted(blogCounts.items(), key=lambda x: x[1], reverse=True)
     newBlogs = [
         f"{blog.replace('scribe.rip', 'medium.com')} ({count})"
-        for blog, count in sortedBlogCounts[:10]
+        for blog, count in sortedBlogCounts[:5]
     ]
     print("\n".join(newBlogs))
     addBlogs = input("Add top 10 blogs to reviewed? (default=no): ")
