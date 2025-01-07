f85734d399994c2b4f52aa7e163b4cc489fef306
distbit <adp01@pm.me>
2025-01-07 18:08:39 +1100

    hello.py pyproject.toml search.sh uv.lock

---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from articlesearchandsync!")
+
+
+if __name__ == "__main__":
+    main()
--- search.sh
+++ search.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-python3 "$SCRIPTPATH"/src/search.py "$@"
+cd "$SCRIPTPATH"
+uv run "$SCRIPTPATH"/src/search.py "$@"
